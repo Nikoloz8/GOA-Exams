5e45abde2278882db2a2b938b4a085cd7de910e4
Nikoloz8 <Nikoloztevdoradze08@gmail.com>
2024-11-26 02:00:13 +0400

    Commit

--- test/Group 44 CW_HW/Day 01/homework/homework01.py
+++
@@ -1,6 +0,0 @@
-name = "Nikoloz"
-surname = "Tevdoradze"
-age = "16"
-height = "160cm" 
-
-print("My name is " + name + " " + surname + ", I am " + age + " years old and my height is " + height + " centimeters.")
